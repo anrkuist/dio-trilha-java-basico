f308b088cefbaa8da5d3d86772eb23e825f96613
anrkuist <anrkuist@protonmail.com>
2025-08-05 17:30:20 -0300

    feat: adicionar entrada de dados para criação de conta bancária via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Importando a classe Scanner
+        java.util.Scanner terminal = new java.util.Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = terminal.nextInt();
+        terminal.nextLine(); // limpa o buffer para evitar problemas com o próximo next()
+
+        System.out.println("Por favor, digite o número da agência:");
+        String numeroAgencia = terminal.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = terminal.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldoConta = terminal.nextDouble();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+                nomeCliente, numeroAgencia, numeroConta, saldoConta);
+        terminal.close();
     }
 }
